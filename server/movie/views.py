d75ad6ca8cf3f4b7d11c591aa2c569250ed927c9
YSHgroup <david.dev100@outlook.com>
2024-03-07 21:44:18 -0500

    Create views

--- server/movie/views.py
+++ server/movie/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Movie
+from .serializers import MovieSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+
+class MovieListCreateView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class AllMoviesListView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    partial = True
+
+class MovieDeleteView(generics.DestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({'message': 'delete Movie'}, status=204)
